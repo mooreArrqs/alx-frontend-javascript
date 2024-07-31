1ab0c0192e231e397e8aed5f51824c80a4ca98a4
mooreArrqs <amandanxumalo921@gmail.com>
2024-07-31 13:32:40 +0000

    Basic list of objects

---
+++ 0x03-ES6_data_manipulation/0-main.js
@@ -0,0 +1,5 @@
+// 0-main.js
+const getListStudents = require('./src/0-get_list_students');
+
+const students = getListStudents();
+console.log(students);
---
+++ 0x03-ES6_data_manipulation/tests/0-get_list_students.test.js
@@ -0,0 +1,15 @@
+// tests/0-get_list_students.test.js
+const getListStudents = require('../src/0-get_list_students');
+
+describe('getListStudents', () => {
+  test('returns an array of student objects', () => {
+  const students = getListStudents();
+  expect(students).toHaveLength(3);
+
+  expect(students).toStrictEqual();
+  expect.arrayContaining([
+    expect.objectContaining({ id: 1, firstName: 'Guillaume', location: 'San Francisco' }),
+    expect.objectContaining({ id: 2, firstName: 'James', location: 'Columbia' }),
+  expect.objectContaining({ id: 5, firstName: 'Serena', location: 'San Francisco' }),
+    ]);
+});
